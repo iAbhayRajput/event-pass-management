c8cd16dd0603a6735070305018ad5e362de2dd4c
iAbhayRajput <abhayravirajput@gmail.com>
2025-02-24 03:36:56 +0530

    added cors

--- frontend/qr-pass-frontend/vite.config.js
+++ frontend/qr-pass-frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const backendUrl = process.env.VITE_BACKEND_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: backendUrl,
+                target: "http://localhost:5000",
                 changeOrigin: true,
                 secure: false
             }
